75dbb4387fb119f4c6745d5652ad7575c46895ea
DamonFlick <75501983+DamonFlick@users.noreply.github.com>
2021-02-23 20:22:09 -0800

    Add project files.

---
+++ Operations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_4
+{
+    class Operations
+    {
+        public static void operate(int x, int y)
+        {
+            x = x * 90;
+            Console.WriteLine("The first interger had some math done on it, but remains hidden. Heres integer two: " + y);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter two numbers, one after the other:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            Operations.operate(x: x, y: y);
+        }
+    }
+}
